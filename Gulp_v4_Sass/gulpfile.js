38736bb054c1eb5de399441ad5ee2a8b7468347f
lightxlamp <sheveliukhin.s@gmail.com>
2020-03-11 17:11:22 +0600

    Gulp_v4 and SCSS working now <3

---
+++ Gulp_v4_Sass/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// compile scss into css
+// gulp.task - used to be in prev. versions
+// now we need to use functions
+
+function style(){
+    // 1) Where is my SCSS? 
+    return gulp.src('./scss/**/*.scss')
+    // 2) Pass that file through sass compiler 
+        .pipe(sass())
+    // 3) Where do I save decompiled css? 
+        .pipe(gulp.dest('./css'))
+}
+
+exports.style = style;
+
